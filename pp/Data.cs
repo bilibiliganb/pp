2306d028583841514e92cb9ae495b6b7d628ee9d
bilibiliganb <49602647+bilibiliganb@users.noreply.github.com>
2021-01-10 12:42:19 +0800

    Add files via upload

---
+++ pp/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp
+{
+    class Data
+    {
+        public static string UID = "", UName = "";//登录名的ID和姓名
+    }
+}
